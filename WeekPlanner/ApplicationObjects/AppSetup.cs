cb231f858e2bdc58a80435fde3accc6d0701d7a8
Casper Thomsen <casper.thomsen100@gmail.com>
2018-04-30 13:34:02 +0200

    Fixed System.Exception

--- WeekPlanner/ApplicationObjects/AppSetup.cs
+++ WeekPlanner/ApplicationObjects/AppSetup.cs
@@ -10,52 +10,54 @@ using WeekPlanner.Services;
 
 namespace WeekPlanner.ApplicationObjects
 {
-    public class AppSetup
-    {
-        public IContainer CreateContainer()
-        {
-            var containerBuilder = new ContainerBuilder();
-            RegisterDependencies(containerBuilder);
-            return containerBuilder.Build();
-        }
+	public class AppSetup
+	{
+		public IContainer CreateContainer()
+		{
+			var containerBuilder = new ContainerBuilder();
+			RegisterDependencies(containerBuilder);
+			return containerBuilder.Build();
+		}
 
-        protected virtual void RegisterDependencies(ContainerBuilder cb)
-        {
-            // *** Constant Registrations ***
-            // ViewModels
-            cb.RegisterType<ChooseCitizenViewModel>();
-            cb.RegisterType<LoginViewModel>();
-            cb.RegisterType<TestingViewModel>();
-            cb.RegisterType<WeekPlannerViewModel>();
-            cb.RegisterType<ChooseTemplateViewModel>();
-            cb.RegisterType<PictogramSearchViewModel>();
-            cb.RegisterType<ActivityViewModel>();
+		protected virtual void RegisterDependencies(ContainerBuilder cb)
+		{
+			// *** Constant Registrations ***
+			// ViewModels
+			cb.RegisterType<ChooseCitizenViewModel>();
+			cb.RegisterType<LoginViewModel>();
+			cb.RegisterType<TestingViewModel>();
+			cb.RegisterType<WeekPlannerViewModel>();
+			cb.RegisterType<ChooseTemplateViewModel>();
+			cb.RegisterType<PictogramSearchViewModel>();
+			cb.RegisterType<ActivityViewModel>();
+			cb.RegisterType<WeekplannerChangeColourViewModel>();
 
-            // Services
-            cb.RegisterType<NavigationService>().As<INavigationService>();
-            cb.RegisterType<SettingsService>().As<ISettingsService>();
-            cb.RegisterType<DialogService>().As<IDialogService>();
-            cb.RegisterType<RequestService>().As<IRequestService>();
 
-            // *** Conditional Registrations ***
-            if (GlobalSettings.Instance.UseMocks)
-            {
-                cb.RegisterType<MockAccountApi>().As<IAccountApi>();
-                cb.RegisterType<MockDepartmentApi>().As<IDepartmentApi>();
-                cb.RegisterType<MockWeekApi>().As<IWeekApi>();
-                cb.RegisterType<MockPictogramApi>().As<IPictogramApi>();
-                cb.RegisterType<MockLoginService>().As<ILoginService>();
-            }
-            else
-            {
-                var accountApi = new AccountApi {Configuration = {BasePath = GlobalSettings.Instance.BaseEndpoint}};
-                cb.RegisterInstance<IAccountApi>(accountApi);
-                cb.RegisterType<LoginService>().As<ILoginService>();
-                cb.RegisterType<WeekApi>().As<IWeekApi>();
-                cb.RegisterType<PictogramApi>().As<IPictogramApi>();
-                cb.RegisterType<DepartmentApi>().As<IDepartmentApi>();
-                cb.RegisterType<PictogramApi>().As<IPictogramApi>();
-            }
-        }
-    }
+			// Services
+			cb.RegisterType<NavigationService>().As<INavigationService>();
+			cb.RegisterType<SettingsService>().As<ISettingsService>();
+			cb.RegisterType<DialogService>().As<IDialogService>();
+			cb.RegisterType<RequestService>().As<IRequestService>();
+
+			// *** Conditional Registrations ***
+			if (GlobalSettings.Instance.UseMocks)
+			{
+				cb.RegisterType<MockAccountApi>().As<IAccountApi>();
+				cb.RegisterType<MockDepartmentApi>().As<IDepartmentApi>();
+				cb.RegisterType<MockWeekApi>().As<IWeekApi>();
+				cb.RegisterType<MockPictogramApi>().As<IPictogramApi>();
+				cb.RegisterType<MockLoginService>().As<ILoginService>();
+			}
+			else
+			{
+				var accountApi = new AccountApi { Configuration = { BasePath = GlobalSettings.Instance.BaseEndpoint } };
+				cb.RegisterInstance<IAccountApi>(accountApi);
+				cb.RegisterType<LoginService>().As<ILoginService>();
+				cb.RegisterType<WeekApi>().As<IWeekApi>();
+				cb.RegisterType<PictogramApi>().As<IPictogramApi>();
+				cb.RegisterType<DepartmentApi>().As<IDepartmentApi>();
+				cb.RegisterType<PictogramApi>().As<IPictogramApi>();
+			}
+		}
+	}
 }
